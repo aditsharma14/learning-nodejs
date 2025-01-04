c7b08079bc6f5f3f4ce1011d72ca91717cda29e9
aditsharma14 <as047385.sharmaadit@gmail.com>
2025-01-04 20:37:44 +0530

    This is my first commit on github while learning nodejs
    Learning nodejs and pushing into my Github

---
+++ express routing basics 2.js
@@ -0,0 +1,15 @@
+import express from "express";
+const app=express();
+app.get("/",(req,res)=>{
+    res.send("<h1>HOME 🏠 </h1>");
+});
+app.get("/about",(req,res)=>{
+    res.send("<h1>ABOUT 😶‍🌫️ </h1>");
+});
+app.get("/contact",(req,res)=>{
+    res.send("<h1>CONTACT 📲 </h1>");
+});
+app.get("/work",(req,res)=>{
+    res.send("<h1>MY WORK 💪</h1>");
+});
+app.listen(8000,()=>console.log("server up!"));
